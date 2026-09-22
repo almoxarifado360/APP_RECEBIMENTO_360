5837a9df50027434ee542c21f42fe78687e520cd
Estoque 360 <almoxarifado.mgtecbombas@gmail.com>
2026-09-22 14:47:08 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "recebimento-360-v1";
+const CACHE_NAME = "recebimento-360-v2";
+
 const APP_FILES = [
   "./",
   "./index.html",
@@ -12,8 +13,11 @@ const APP_FILES = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll(APP_FILES)
+    )
   );
+
   self.skipWaiting();
 });
 
@@ -27,6 +31,7 @@ self.addEventListener("activate", event => {
       )
     )
   );
+
   self.clients.claim();
 });
 
@@ -35,11 +40,29 @@ self.addEventListener("fetch", event => {
 
   event.respondWith(
     caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match("./index.html"));
+
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(event.request)
+        .then(response => {
+
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache =>
+              cache.put(
+                event.request,
+                copy
+              )
+            );
+
+          return response;
+        })
+        .catch(() =>
+          caches.match("./index.html")
+        );
     })
   );
 });
